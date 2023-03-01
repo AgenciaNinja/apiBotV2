789b029ffee530bbb356406d711b557f9f37ea06
Juan Figuera <juanrfv@gmail.com>
2023-03-01 16:17:36 +0100

    act

--- application/controllers/Cli.php
+++ application/controllers/Cli.php
@@ -76,7 +76,7 @@ class Cli extends CI_Controller
             $this->db->save_queries = false;
             $wheres = ["tipo" => "correo","enviado" => "pendiente"];
             $emails = $this->generic_model
-                ->getMultipleBy("informacion_contacto", "*", $wheres, false, false, 10);
+                ->getMultipleBy("informacion_contacto", "*", $wheres, false, false, 1);
             foreach ($emails as $email) {
                 echo $email->valor.PHP_EOL;
                 $this->curlOpts['CURLOPT_POSTFIELDS']['email'] = $email->valor;
