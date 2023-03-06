336a70365341f28ef51d8f48ef651c00568fa1a3
Juan Figuera <juanrfv@gmail.com>
2023-03-06 11:11:19 +0100

    act

--- application/controllers/DashBoardResume.php
+++ application/controllers/DashBoardResume.php
@@ -94,8 +94,13 @@ class DashBoardResume extends MY_Controller
         $this->generic_model->setDb($this->input->post("dbName"));
 
         $data = [
+            "id_mensaje"          => 0,
             "estado"              => "pendiente",
-            "fecha_actualizacion" => $actualTime->format("Y-m-d H:i:s")
+            "url_formulario"      => "",
+            "captcha"             => "pendiente",
+            "form"                => "pendiente",
+            "fecha_actualizacion" => "",
+            "fecha_finalizado" => ""
         ];
 
         $wheres = [
@@ -121,84 +126,4 @@ class DashBoardResume extends MY_Controller
             ->updateMultiple("tareas", $wheres, false, $data);
         echo json_encode(["action" => "success", "estado" => "pendiente"]);
     }
-
-    /*public function trabajo()
-    {
-        $this->db->save_queries = false;
-        $tomorrow = new DateTime('+1 day');
-        $conds = [
-            'estado'          => 'finalizado',
-            'fecha_inicio >=' => date("Y-m-d"),
-            'fecha_inicio <'  => $tomorrow->format('Y-m-d'),
-        ];
-        $data['titlePag'] = 'Trabajo - Control';
-        $data['trabajos'] = $this->trabajos_model
-            ->getMultipleBy("*", $conds, false, "web", "web");
-        //$data['pedientes']   = [];
-        $this->__vista('frontend/trabajo', $data);
-    }*/
-
-    /*private function getEstructura()
-    {
-        $minutos = [];
-        $horaIni = 6;
-        $horaFin = 22;
-        for ($i =  $horaIni; $i < $horaFin; $i++) {
-            for ($ii =  0; $ii < 60; $ii++) {
-                $minutos[($i < 10 ? "0".$i : $i).":".($ii < 10 ? "0".$ii : $ii)] = [
-                    'desktop' => 0,
-                    'mobile'  => 0,
-                    'tablet'  => 0
-                ];
-            }
-        }
-        return $minutos;
-    }*/
-
-    /*public function dataVisitasAjax()
-    {
-        $web  = $this->input->post("web");
-        $data = [
-            'web'   => $web,
-            'fecha' => date("d-M-Y")
-        ];
-        $next = new DateTime('+1 day');
-        $conds= [
-            'estado'          => 'finalizado',
-            'web'             => $web,
-            'fecha_inicio >=' => date("Y-m-d"),
-            'fecha_inicio <'  => $next->format('Y-m-d')
-        ];
-
-        $trabajos = $this->trabajos_model
-            ->getMultipleBy("id", $conds, false);
-        if ($trabajos) {
-            $ids = [];
-            foreach ($trabajos as $trabajo) {
-                $ids[] = $trabajo->id;
-            }
-
-            $visitas = $this->visitas_model->getVisitasByTrabajos($ids);
-            if ($visitas) {
-                $contadores =  $this->getEstructura();
-                foreach ($visitas as $visita) {
-                    $indice1 = trim(substr($visita->fecha, 10, 6));
-                    $indice2 = $visita->dispositivo;
-
-                    $contador  = $contadores[$indice1];
-                    $contador[$indice2] = $contador[$indice2] +1 ;
-                    $contadores[$indice1] = $contador;
-                }
-                // clasificar por dispositivo
-                foreach ($contadores as $key  => $contador) {
-                    $data['categorias'][] = $key;
-                    $data['desktop'][]= $contador['desktop'];
-                    $data['mobile'][] = $contador['mobile'];
-                    $data['tablet'][] = $contador['tablet'];
-                }
-            }
-        }
-        echo json_encode($data);
-    }*/
-
 }
